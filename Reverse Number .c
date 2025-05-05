44f892f79de48f46af8b8c5c0c3a4eeca6cc14c3
kurmirajendra <rajendrakurmi78@gmail.com>
2025-05-05 21:31:26 +0530

    Create Reverse Number .c
    This C program reverses the digits of an integer using a while loop. It extracts digits from the number and builds the reverse number without using if-else or switch statements.

---
+++ Reverse Number .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int num,reverse=0;
+    printf("Enter a number ");
+    scanf("%d",&num);
+    while(num!=0)
+    {
+       reverse=reverse*10+num%10;
+       num=num/10;
+    }
+    printf("Reversed number = %d\n",reverse);
+}
